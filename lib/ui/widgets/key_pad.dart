dd90c43fccbb2f8cf82c82692c63b42ca28eb0b4
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2023-05-24 09:13:58 -0500

    add more guide code

--- lib/ui/widgets/key_pad.dart
+++ lib/ui/widgets/key_pad.dart
@@ -19,8 +19,50 @@ class KeyPad extends StatefulWidget {
 }
 
 class _KeyPadState extends State<KeyPad> {
+  double _currency2 = 0;
+  int _currency1 = 0;
+
+  void _onPressed(int k) {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const OneKey(number: 1);
+    return Column(
+      children: [
+        Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                widget.textCurrency1,
+                key: const Key('TextCurrency1'),
+              ),
+            ),
+            Expanded(
+              child: Container(),
+            ),
+            Container(
+                padding: const EdgeInsets.all(8.0),
+                alignment: Alignment.centerRight,
+                child: Text(
+                  _currency1.toString(),
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.secondary,
+                      fontSize: 35),
+                )),
+          ],
+        ),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              OneKey(number: 7, callback: _onPressed),
+              OneKey(number: 8, callback: _onPressed),
+              OneKey(number: 9, callback: _onPressed),
+            ])
+      ],
+    );
   }
 }
--- lib/ui/widgets/one_key.dart
+++ lib/ui/widgets/one_key.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
 class OneKey extends StatelessWidget {
-  const OneKey({Key? key, required this.number}) : super(key: key);
+  const OneKey({Key? key, required this.number, required this.callback})
+      : super(key: key);
+
   final int number;
+  final Function(int) callback;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      key: Key(number.toString()),
-      child: Text(number.toString()),
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+        child: MaterialButton(
+            key: Key(number.toString()),
+            color: Theme.of(context).primaryColor,
+            padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+            onPressed: () {
+              callback(number);
+            },
+            child: Text(number.toString(),
+                style: const TextStyle(
+                  fontSize: 26.0,
+                  color: Colors.white,
+                ))),
+      ),
     );
   }
 }
